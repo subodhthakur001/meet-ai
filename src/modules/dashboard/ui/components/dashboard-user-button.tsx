9995d5dcf85f45083a119972a5b3b45a28555e5f
subodh <skthakur.styles619@gmail.com>
2025-09-27 16:59:14 +0530

    added a new sidebar component and logout logic

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -2,13 +2,25 @@ import { Avatar } from "@/components/ui/avatar";
 import { GeneratedAvatar } from "@/components/ui/generated-avatar";
 import { authClient } from "@/lib/auth-client";
 import { AvatarImage } from "@radix-ui/react-avatar";
-import { DropdownMenu, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const DashboardUserButton = () => {
     const {data , isPending} = authClient.useSession();
+    const router = useRouter();
 
     if(isPending || !data?.user){
         return null;
+    }
+    const signOut = () => {
+        authClient.signOut({
+            fetchOptions: {
+                onSuccess : () => {
+                    router.push("/sign-in");
+                }
+            }
+        })
     }
     return (
         <DropdownMenu>
@@ -27,7 +39,30 @@ const DashboardUserButton = () => {
                 />
             )
           }
+          <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0 m-2">
+            <p className="text-sm truncate w-full">{data.user.name}</p>
+            <p className="text-xs truncate w-full">{data.user.email}</p>
+          </div>
+          <ChevronDownIcon className="size-4 shrink-0"/>
             </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" side="right" className="w-72">
+                <DropdownMenuLabel>
+                    <div className="flex flex-col gap-1">
+                        <span className="font-medium truncate">{data.user.name}</span>
+                        <span className="text-sm font-normal text-muted-foreground truncate">{data.user.email}</span>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator/>
+               <DropdownMenuItem className="cursor-pointer flex items-center justify-between">
+                Billing
+                <CreditCardIcon className="size-4"/>
+               </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer flex items-center justify-between" onClick={signOut}>
+                    Logout
+                <LogOutIcon className="size-4"/>
+               </DropdownMenuItem>
+            </DropdownMenuContent>
+          
         </DropdownMenu>
     )
 }
