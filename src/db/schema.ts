11206d5cb3628e3c0f88be03a4208886ce5db6c9
subodh <skthakur.styles619@gmail.com>
2025-09-29 22:57:55 +0530

    created agents schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 
-import { pgTable, text, timestamp, boolean } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, boolean, time } from "drizzle-orm/pg-core";
+import { nanoid } from "nanoid";
 
 export const user = pgTable("user", {
   id: text("id").primaryKey(),
@@ -60,3 +61,13 @@ export const verification = pgTable("verification", {
     .$onUpdate(() => /* @__PURE__ */ new Date())
     .notNull(),
 });
+
+export const agents = pgTable("agents",{
+  id : text("id").primaryKey().$defaultFn(() => nanoid()),
+  name : text("name").notNull(),
+  userId : text("user_id").notNull().references(() => user.id),
+  instructions: text("instructions").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+
+})
