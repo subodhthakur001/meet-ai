181d881f0e881e30bc8c43c7ea0c5b2983c56893
subodh <skthakur.styles619@gmail.com>
2025-09-30 23:27:51 +0530

    created protected trpc procedure

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,10 +1,12 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { createTRPCRouter, baseProcedure, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
+import { AgentsInsertSchema } from "../schema";
 
 
 export const agentsRouter = createTRPCRouter({
+    // Todo : replace baseProcedure with ProtectedProceduere 
     getMany: baseProcedure.query(async() => {
         const data = await db.
         select()
@@ -12,5 +14,14 @@ export const agentsRouter = createTRPCRouter({
     // await new Promise((resolve) => setTimeout(resolve, 5000));
     // throw new TRPCError({code : "BAD_REQUEST"})
         return data;
-    })
+    }),
+    create: protectedProcedure
+    .input(AgentsInsertSchema)
+    .mutation(async ({input, ctx}) => {
+        const [createdAgent] = await db
+        .insert(agents)
+        .values({...input, userId: ctx.auth.user.id})
+        .returning();
+        return createdAgent;
+    }) 
 })
